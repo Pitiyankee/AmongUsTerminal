24935264826ba84a0739e862950f20cf5d455a1e
Carlos <gamliel.zulhe@gmail.com>
2026-03-13 16:44:34 +0100

    Añado la interfaz Saboteable y la clase Tarea

---
+++ src/main/java/Saboteable.java
@@ -0,0 +1,8 @@
+// Pedro, bro, aqui te dejo la interfaz que pidio Farid.
+// ahora ya no te dara error cuando le quites las barritas // a tu codigo en tu PC.
+public interface Saboteable {
+
+    // Solo pongo el nombre de la accion, tu Impostor en tu computadora es el que decide como hacerla
+    void sabotear(Sala sala);
+
+}
